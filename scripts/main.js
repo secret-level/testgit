379330354d1e3482212b1de671c5d6c19dd58448
secret-level <vizjaqtaar@hotmail.com>
2020-09-29 21:56:49 +0200

    added rows

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,9 @@ async function getData()
   const response = await fetch('zonal.csv');
   const data = await response.text();
   console.log(data);
+  
+  const rows = data.split('\n');
+  console.log(rows);
 }
 
 getData();
