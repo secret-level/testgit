ab3128a15b08f33cebd8726f62abc749d5cc7d14
secret-level <vizjaqtaar@hotmail.com>
2020-09-30 18:05:59 +0200

    new chartIt()

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,8 @@ async function getData()
 {
   const response = await fetch('zonal.csv');
   const data = await response.text();
-  console.log(data);
   
-  const rows = data.split('\n').slice(1);
+  const table = data.split('\n').slice(1);
   table.forEach(row =>
   {
     const columns = row.split(',');
@@ -15,26 +14,26 @@ async function getData()
     const temp = columns[1];
     console.log(year, temp);
   });
-  
+}
+
+function chartIt()
+{
   const ctx = document.getElementById('chart').getContext('2d');
   const xlabels = [];
   const chart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: xlabels,
-        datasets: [{
-            label: 'Average temperature',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-            ],
-            borderWidth: 1
-        }]
+      labels: xlabels,
+      datasets: [{
+        label: 'Average temperature',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)'],
+        borderWidth: 1
+      }]
     },
   });
 }
 
 getData();
+chartIt();
