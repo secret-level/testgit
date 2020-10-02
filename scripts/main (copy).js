e419cd07d03a6c8bb28de6e3eb7cc6ff6ced4635
secret-level <vizjaqtaar@hotmail.com>
2020-10-02 17:20:30 +0200

    backtracking

--- scripts/main (copy).js
+++ scripts/main (copy).js
@@ -1,15 +1,12 @@
 window.addEventListener('load', chartIt);
 
-const xs = [];
-const ys = []; 
-
 async function getData()
 { 
   const response = await fetch('zonal.csv');
   const stuff = await response.text();
  
-  //const xs = [];
-  //const ys = []; 
+  const xs = [];
+  const ys = []; 
   const table = stuff.split('\n').slice(1);
   table.forEach(row =>
   {
@@ -18,23 +15,20 @@ async function getData()
     ys.push(columns[1]);
     console.log(columns[0], columns[1]);
   });
-  //return (xs, ys);
+  return (xs, ys);
 }
 
 async function chartIt()
 {
-  await getData();
   const ctx = document.getElementById('chart').getContext('2d');
-  //const myData = await getData();
+  const myData = await getData();
   const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      //labels: myData.xs,
-      labels: xs,
+      labels: myData.xs,
       datasets: [{
         label: 'Average temperature',
-        //data: myData.ys,
-        data: ys,
+        data: myData.ys,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1
